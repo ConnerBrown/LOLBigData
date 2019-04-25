6feee002514df05932d3e0e40e1b805b09f24d44
Conner Brown <cbrown18@zagmail.gonzaga.edu>
2019-04-24 20:03:56 -0700

    utils

---
+++ cleaner.py
@@ -0,0 +1,40 @@
+import utils
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    lol = utils.readTable('raw/LeagueofLegends.csv')
+    i = 0
+    for item in lol[0]:
+        print(i , " " , item)
+        i += 1
+
+    utils.convertToNumeric(lol)
+    #remove unused columns
+    # 0League,1Year,2Season,3Type,4blueTeamTag,5bResult,6rResult,7redTeamTag,8gamelength,
+    # 9golddiff,10goldblue,11bKills,12bTowers,13bInhibs,14bDragons,15bBarons,16bHeralds,
+    # 17goldred,18rKills,19rTowers,20rInhibs,21rDragons,22rBarons,23rHeralds,24blueTop,
+    # 25blueTopChamp,26goldblueTop,27blueJungle,28blueJungleChamp,29goldblueJungle,
+    # 30blueMiddle,31blueMiddleChamp,32goldblueMiddle,33blueADC,34blueADCChamp,
+    # 35goldblueADC,36blueSupport,37blueSupportChamp,38goldblueSupport,39blueBans,40redTop,
+    # 41redTopChamp,42goldredTop,43redJungle,44redJungleChamp,45goldredJungle,46redMiddle,
+    # 47redMiddleChamp,48goldredMiddle,49redADC,50redADCChamp,51goldredADC,52redSupport,
+    # 53redSupportChamp,54goldredSupport,55redBans,56Address
+    indexesToRemove = [0,1,2,3,4,7,10,17,26,29,32,35,37,39, 42,45, 48, 51, 54, 55, 56]
+    lolColRemoved = utils.removeColumns(lol, indexesToRemove)
+    print(lol[1])
+    print(lolColRemoved[1])
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,239 @@
+#Utility functions
+import math
+import copy
+import tabulate
+import random
+
+
+#removes columns from a table
+def removeColumns(table, indexes):
+    new_table = []
+    row = []
+    for item in table:
+        for index in range(len(table[0])):
+            if index not in  indexes:
+                row.append(item[index])
+        new_table.append(row)
+        row = []
+    return new_table
+
+
+#reads a csv file of the given name into a list
+#returns said list
+def readTable(filename):
+    table = []
+    myFile = open(filename, 'r')
+    #read each line in file and append into the table
+    lines = myFile.read().split('\n')
+    for line in lines:
+        values = line.split(',')
+        table.append(values)
+    myFile.close()
+    return table
+	
+# Converts the specified column 
+def convertColToInt(table, colIndex, header):
+    for i in range(header, len(table), 1):
+        try:
+            table[i][colIndex] = int(table[i][colIndex])
+        except:
+            continue
+
+#takes a table of values and turns them into floats
+#'NA' values and alpha character strings are left in the list
+def convertToNumeric(table):
+    #try to convert to int
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            try:
+                table[i][j] = float(table[i][j])
+            except:
+                continue
+
+			
+#prints a 'table' to a file
+#taken from Gina's Repo
+def writeTable(filename, table):
+    outfile = open(filename, "w")
+    for k in range(len(table)):
+        for i in range(len(table[k])):
+            outfile.write(str(table[k][i]))
+            if i < len(table[k])-1:
+                outfile.write(',')
+        if k < len(table)-1:
+            outfile.write('\n')
+        
+    
+    outfile.close()
+	
+#counts occurrences base on a attribute filter
+def counter(data, column, filt, ignoreHeader):
+    count = 0
+    for row in data:
+        if(filt is not None):
+            if(row[column]==filt):
+                count += 1
+        else:
+            count += 1
+    if(ignoreHeader and count>0):
+        count -= 1
+    return count
+	
+# @Gina's repo
+def getColumn(table, column_index):
+    column = []
+    for row in table:
+            column.append(row[column_index])
+
+    return column
+	
+# @Gina's Repo
+def getFrequencies(table, column_index):
+    column = getColumn(table, column_index)
+    values = []
+    counts = []
+
+    for value in column:
+        if value not in values:
+            values.append(value)
+            # first time we have seen this value
+            counts.append(1)
+        else: # we've seen it before, the list is not sorted...
+            counts[values.index(value)] += 1
+
+    return values, counts
+	
+# @Gina's Repo
+def groupBy(table, column_index, include_only_column_index=None):
+    # first identify unique values in the column
+    group_names = list(set(getColumn(table, column_index)))
+
+    # now, we need a list of subtables
+    # each subtable corresponds to a value in group_names
+    # parallel arrays
+    groups = [[] for name in group_names]
+    for row in table:
+        # which group does it belong to?
+        group_by_value = row[column_index]
+        index = group_names.index(group_by_value)
+        if include_only_column_index is None:
+            groups[index].append(row.copy()) # note: shallow copy
+        else:
+            groups[index].append(row[include_only_column_index])
+
+    return group_names, groups
+
+
+#generate folds for stratified cross V
+def stratifiedCrossValidationBins(K, table, classIndex):
+    names, values = groupBy(table, classIndex)
+    bins = []
+    for i in range(K):
+        bins.append([])
+    bindex = 0
+    for i in range(len(values)):
+        for j in range(len(values[i])):
+            bins[bindex].append(values[i][j])
+            bindex = (bindex + 1)%10
+
+    return bins 
+
+#convert stratified bins into train/test set
+def binsToSets(bins, index):
+    test = bins[index]
+    train = []
+    for i in range(len(bins)):
+        if not i == index:
+            for j in range(len(bins[i])):
+                train.append(bins[i][j])
+    return train, test
+
+
+#divsion by 0 = 0
+def div(x,y):
+    if y ==0:
+        return 0
+    return x/y
+
+
+#build a tabulated confusion martix
+def build_confusion_matrix(predictions_w_actual, headers, classes, strClassFlg=False):
+    #If the classes are strings they must be converted to integers
+    if strClassFlg==True:
+        str_classes = copy.deepcopy(classes)
+        for i in range(len(classes)):
+            classes[i] = i+1
+        for i in range(len(predictions_w_actual)):
+            for k in range(len(predictions_w_actual[i])):
+                for l in range(len(predictions_w_actual[i][k])):
+                    for j in range(len(str_classes)):
+                        if predictions_w_actual[i][k][l] == str_classes[j]:
+                            predictions_w_actual[i][k][l] = classes[j]
+                        elif predictions_w_actual[i][k][l] == str_classes[j]:
+                            predictions_w_actual[i][k][l] = classes[j]
+
+    value = [0 for i in range(len(classes))]
+    correct = 0
+    incorrect = 0
+    calue_matrix = []
+    for i in range(len(classes)):
+        for j in range(len(predictions_w_actual)):
+            for k in range(len(predictions_w_actual[j][0])):
+                #predictions are at predWA[][0] actuals at predWA[][1]
+                #true, accurate prediction
+                if(classes[i] == predictions_w_actual[j][0][k] 
+                            == predictions_w_actual[j][1][k]):
+                    value[i] += 1
+                    correct += 1
+                #inaccurate prediction
+                elif(classes[i] == predictions_w_actual[j][1][k]):
+                    value[predictions_w_actual[j][0][k]-1] += 1 
+                    incorrect += 1
+        #compute total
+        value.append(correct + incorrect)
+        #compute recog
+        value.append(round(div(correct, correct + incorrect),2))
+        correct = 0
+        incorrect = 0
+        #add class
+        value.insert(0, classes[i])
+        calue_matrix.append(value)
+        #clear value
+        value = [0 for i in range(len(classes))]
+    if strClassFlg:
+        for i in range(len(calue_matrix)):
+            calue_matrix[i][0] = str_classes[calue_matrix[i][0]-1]
+    return(tabulate.tabulate(calue_matrix, headers))
+
+
+#@Gina's repo
+def bootstrap(table):
+    n = len(table)
+    sample = []
+    for _ in range(n):
+        rand_index = random.randrange(0, n)
+        sample.append(table[rand_index])
+
+    return sample
+
+
+#compute majority vote
+def majority_vote(votes):
+    lis = sorted(votes)
+    prime = 0
+    val = 0
+    mode = lis[0]
+    curr = lis[0]
+    for i in range(len(lis)):
+        if lis[i] == curr and i != len(lis)-1:
+            prime +=1
+        else:
+            if prime > val:
+                val = prime
+                mode = curr
+            curr = lis[i]
+            prime = 1
+    return mode
+
+
+
