5f3ffde74f986e27154c63850eba0ac3757b394e
Andrew Italo <aitalo@zagmail.gonzaga.edu>
2019-05-09 16:08:40 -0700

    golddiff

--- data_exploration.py
+++ data_exploration.py
@@ -14,7 +14,11 @@ print("Red:", red.shape[0])
 print("    # -", red.shape[0])
 print("    % -", red.shape[0]/df.shape[0])
 
+print(blue)
+
 df2 = pd.read_csv("LoL_clean_manual.csv")
+blue = df2[df2.bResult == 1]
+red = df2[df2.bResult == 0]
 print(df2.shape[0])
 length_l = df[["gamelength"]]
 length_l = length_l.values.tolist()
@@ -25,3 +29,39 @@ print("Shortest game:", min(length))
 print("Longest Game:", max(length))
 average = sum(length) / len(length)
 print("Average:", average)
+
+gold_r = red[["Minute", "golddiff"]].values.tolist()
+gold_b = blue[["Minute", "golddiff"]].values.tolist()
+
+
+
+gold_minute = {}
+for i in range(len(gold_r)):
+    if gold_r[i][0] in gold_minute:
+        gold_minute[gold_r[i][0]].append(-gold_r[i][1])
+    else:
+        gold_minute[gold_r[i][0]] = [-gold_r[i][1]]
+
+for i in range(len(gold_b)):
+    if gold_b[i][0] in gold_minute:
+        gold_minute[gold_b[i][0]].append(gold_b[i][1])
+    else:
+        gold_minute[gold_b[i][0]] = [gold_b[i][1]]
+print(gold_minute[75])
+
+for x in gold_minute:
+    gold_minute[x] = sum(gold_minute[x]) / len(gold_minute[x])
+
+averages = []
+i = 1
+while i in gold_minute:
+    averages.append(gold_minute[i])
+    i += 1
+y = [i+1 for i in range(len(averages)-1)]
+print
+plt.plot(y, averages[:74])
+plt.xlabel("minute")
+plt.ylabel("golddiff")
+plt.title("Average gold lead of the winning team by minute")
+plt.show()
+
